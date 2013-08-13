73311113f46dc65be5de5d1adb30f52eb97f856f
Albert Cervera i Areny <albert@nan-tic.com>
2013-08-13 14:01:03 +0200

    Remove colletions.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,3 @@
-from invoke import Collection
-
-from . import bootstrap
-from . import utils
-from . import scm
-
-ns = Collection()
-ns.add_collection(Collection.from_module(bootstrap))
-ns.add_collection(Collection.from_module(utils))
-ns.add_collection(Collection.from_module(scm))
+from .bootstrap import *
+from .utils import *
+from .scm import *
