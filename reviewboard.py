ddbb7e3248de6f33305e955d7138a8701fe8be04
Àngel Àlvarez <angel@nan-tic.com>
2014-03-13 17:08:57 +0100

    updload diff

--- reviewboard.py
+++ reviewboard.py
@@ -2,26 +2,46 @@
 
 from invoke import task, run
 from .scm import module_diff
+from .config import get_config
+import ConfigParser
 
+
+try:
+    from rbtools.api.client import RBClient
+except:
+    pass
+
+
+def get_root():
+    settings =  get_config()
+    review = settings['reviewboard']
+    client = RBClient(review['server'])
+    client.login(review['user'], review['password'])
+    return client.get_root()
 
 def get_repository():
-    from rbtools.api.client import RBClient
-    client = RBClient('http://git.nan-tic.com/reviews/', user='angel',
-        password='')
-    root = client.get_root()
+    root = get_root()
     repository = root.get_repositories()[-1].id
+    return repository
 
 @task
-def upload_diff(review=None):
-    diff, base_diff = module_diff('tasks')
-    review_request = root.get_review_requests().create(
-        repository=get_repository())
+def review(module, summary, description, bug, review=None):
+    diff, base_diff = module_diff(module)
+    root = get_root()
+    if review:
+        review_request = root.get_review_request(review_request_id=review)
+    else:
+        review_request = root.get_review_requests().create(
+            repository=get_repository())
     review_request.get_diffs().upload_diff(diff, base_diff)
     draft = review_request.get_draft()
     draft.update(
-        summary='prova',
-        description='prova',
+        summary=summary,
+        description=description,
+        bugs_closed=bug,
         )
     user = root.get_session().get_user()
     draft = draft.update(target_people=user.username)
     draft.update(public=True)
+
+def
